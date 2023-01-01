74c258a30cc559d3e49d66a4058ffb518b351514
Dave Carbonara <carbonara@Daves-MacBook-Pro.local>
2023-01-01 17:50:05 +0100

    Migrated to Redux

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -73,7 +73,7 @@ const SignInForm = () => {
             type='button'
             onClick={signInWithGoogle}
           >
-            Google Sign In
+            Sign In With Google
           </Button>
         </ButtonsContainer>
       </form>
